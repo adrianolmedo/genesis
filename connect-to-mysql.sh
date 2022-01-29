fe828efb902b0c5bdaa801e4c6baf1823add630c
Adrián Olmedo <adrianolmedo.ve@gmail.com>
2022-01-29 00:08:15 -0400

    All JSON config moved to connect-to-[...].sh files

---
+++ connect-to-mysql.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+./rest -port 1323 -cors 127.0.0.1 \
+    -dbengine mysql \
+    -dbhost 127.0.0.1 \
+    -dbport 3306 \
+    -dbuser pmadrian \
+    -dbpass 3eD5gfiqjSYO@x%k \
+    -dbname go_practice_restapi
