d3eee983a69b060305e0d35f33a7e4425eaefe9b
Adrián Olmedo <adrianolmedo.ve@gmail.com>
2022-02-06 07:20:06 -0400

    Add invoice_headers and invoice_items tables

--- .docker/mysql/database.sql
+++ .docker/mysql/database.sql
@@ -23,3 +23,20 @@ CREATE TABLE IF NOT EXISTS products (
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS invoice_headers (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    client_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS invoice_items (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    invoice_header_id INT NOT NULL,
+    product_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP,
+    CONSTRAINT invoice_items_invoice_header_id_pk FOREIGN KEY (invoice_header_id) REFERENCES invoice_headers (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+    CONSTRAINT invoice_items_product_id_pk FOREIGN KEY (product_id) REFERENCES products (id) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
