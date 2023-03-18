f5017f588ff185b1d56133b1640597f217c16911
Adrian Olmedo <adrianolmedo.ve@gmail.com>
2023-03-18 16:56:13 -0400

    Empty branch

--- internal/rest/reqmeth_test.go
+++
@@ -1,9 +0,0 @@
-package rest
-
-import "github.com/labstack/echo/v4"
-
-// reqMeth simulate func signing of HTTP request methods of Echo framework
-// like GET, POST, PUT, DELETE, etc.
-func reqMeth(c echo.Context, h echo.HandlerFunc) error {
-	return h(c)
-}
--- run-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for d in $(go list ./...); do
-	echo "testing package $d"
-	go test -v $d
-done
