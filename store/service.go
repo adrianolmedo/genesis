fdb0919e58a8d89f26d6dffeb5906e89a2787090
Adrian Olmedo <adrianolmedo.ve@gmail.com>
2023-03-31 13:39:42 -0400

    Add more methods for Service

--- store/service.go
+++ store/service.go
@@ -20,10 +20,40 @@ func (s Service) Add(product *Product) error {
 }
 
 // addProductService business logic for adding products to the store.
+// The business logic has been split into a smaller function for unit testing
+// purposes, and it should do so for the other methods of the Service.
 func addProductService(p *Product) error {
 	if !p.HasName() {
 		return ErrProductHasNoName
 	}
 
 	return nil
+}
+
+func (s Service) Find(id int64) (*Product, error) {
+	if id == 0 {
+		return &Product{}, ErrProductNotFound
+	}
+
+	return s.repo.ByID(id)
+}
+
+func (s Service) Update(product Product) error {
+	if !product.HasName() {
+		return ErrProductHasNoName
+	}
+
+	return s.repo.Update(product)
+}
+
+func (s Service) List() ([]*Product, error) {
+	return s.repo.All()
+}
+
+func (s Service) Remove(id int64) error {
+	if id == 0 {
+		return ErrProductNotFound
+	}
+
+	return s.repo.Delete(id)
 }
