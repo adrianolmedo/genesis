fb2924c7501c959d1f2cea24616f1fa1d221d1f3
Adrian Olmedo <adrianolmedo.ve@gmail.com>
2023-03-31 14:06:38 -0400

    Separate business logic into a small func for unit testing purposes

--- user/service.go
+++ user/service.go
@@ -10,6 +10,33 @@ func NewService(repo Repository) Service {
 	}
 }
 
+// SignUp to register a User.
+func (s Service) SignUp(user *User) error {
+	err := signUpService(user)
+	if err != nil {
+		return err
+	}
+
+	return s.repo.Create(user)
+}
+
+// signUpService business logic for regitser a User. Has been split into
+// a smaller function for unit testing purposes, and it should do so for
+// the other methods of the Service.
+func signUpService(user *User) error {
+	err := user.CheckEmptyFields()
+	if err != nil {
+		return err
+	}
+
+	err = validateEmail(user.Email)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // Find a User by its ID.
 func (s Service) Find(id int64) (*User, error) {
 	if id == 0 {
