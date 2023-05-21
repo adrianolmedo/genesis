8b8dd5fe520d619652c06aff01381e32f220ea1d
Adrian Olmedo <adrianolmedo.ve@gmail.com>
2023-05-21 13:54:04 -0400

    Fix doc comment

--- internal/rest/routes.go
+++ internal/rest/routes.go
@@ -14,8 +14,8 @@ func Routes(e *echo.Echo, s service.Service) {
 	e.GET("/v1/users/:id", findUser(s))
 
 	u := e.Group("/v1/users")
-	u.Use(middleware.Auth)  // Routes that required authentication.
-	u.GET("", listUsers(s)) // E.g.: GET /v1/users
+	u.Use(middleware.Auth) // Routes that required authentication.
+	u.GET("", listUsers(s))
 	u.PUT("/:id", updateUser(s))
 	u.DELETE("/:id", deleteUser(s))
 
