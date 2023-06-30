d7fac1be8365f008888f9b2302d33a72bf22f499
Adrian Olmedo <adrianolmedo.ve@gmail.com>
2023-06-30 03:30:47 -0400

    Fix Direction enum

--- filter.go
+++ filter.go
@@ -4,11 +4,11 @@ package genesis
 type Direction int
 
 const (
-	// DESC filter results descending.
-	DESC Direction = iota
-
 	// ASC filter results ascending.
-	ASC
+	ASC Direction = iota
+
+	// DESC filter results descending.
+	DESC
 )
 
 func (d Direction) String() string {
@@ -34,6 +34,17 @@ type Filter struct {
 	MaxLimit int
 }
 
+// FilterResults model that provide the filtered results and its data for
+// build the pagination.
+type FilterResults struct {
+	// Rows subset of results, not all of results.
+	Rows       interface{}
+	TotalRows  int
+	TotalPages int
+	FromRow    int
+	ToRow      int
+}
+
 // PaginationLinks it's a DTO to complies with the HATEOAS principle to display
 // the information needed to create pagination.
 type PaginationLinks struct {
@@ -48,15 +59,4 @@ type PaginationLinks struct {
 	LastPage     string `json:"last_page"`
 	FromRow      int    `json:"from_row"`
 	ToRow        int    `json:"to_row"`
-}
-
-// FilterResults model that provide the filtered results and its data for
-// build the pagination.
-type FilterResults struct {
-	// Rows subset of results, not all of results.
-	Rows       interface{}
-	TotalRows  int
-	TotalPages int
-	FromRow    int
-	ToRow      int
 }
