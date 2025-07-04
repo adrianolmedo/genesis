9e2aaa4a788efa2640581d159d9a64026741126c
Adrian Olmedo <adrianolmedo.ve@gmail.com>
2025-07-04 07:47:53 -0400

    Refactor user SQL queries to include RETURNING id for update and delete operations

--- pgsql/sqlc/queries/user.sql
+++ pgsql/sqlc/queries/user.sql
@@ -9,14 +9,20 @@ SELECT id FROM "user" WHERE email = $1 AND password = $2 AND deleted_at IS NULL;
 -- name: UserByID :one
 SELECT * FROM "user" WHERE id = $1 AND deleted_at IS NULL;
 
--- name: UserUpdate :exec
-UPDATE "user" SET first_name = $1, last_name = $2, email = $3, password = $4, updated_at = $5 WHERE id = $6;
+-- name: UserUpdate :one
+UPDATE "user" SET first_name = $1, last_name = $2, email = $3, password = $4, updated_at = $5 WHERE id = $6 RETURNING id;
 
--- name: UserDelete :exec
-UPDATE "user" SET deleted_at = $1 WHERE id = $2;
+-- name: UserDelete :one
+UPDATE "user" SET deleted_at = $1 WHERE id = $2 RETURNING id;
 
--- name: UserHardDelete :exec
-DELETE FROM "user" WHERE id = $1;
+-- name: UserHardDelete :one
+DELETE FROM "user" WHERE id = $1 RETURNING id;
 
 -- name: UserDeleteAll :exec
 TRUNCATE TABLE "user" RESTART IDENTITY;
+
+-- name: UserListAsc :many
+SELECT * FROM "user" WHERE deleted_at IS NULL ORDER BY @sort::text ASC LIMIT $1 OFFSET $2;
+
+-- name: UserListCount :one
+SELECT COUNT (*) FROM "user" WHERE deleted_at IS NULL;
