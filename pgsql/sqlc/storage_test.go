d806bd7a942dbfb4f587c4ff052d18dc3d5e7630
Adrian Olmedo <adrianolmedo.ve@gmail.com>
2025-07-04 07:42:02 -0400

    Add firts tests

---
+++ pgsql/sqlc/storage_test.go
@@ -0,0 +1,52 @@
+//go:build integration
+// +build integration
+
+package sqlc
+
+import (
+	"context"
+	"flag"
+	"testing"
+
+	config "github.com/adrianolmedo/genesis"
+
+	"github.com/jackc/pgx/v5"
+)
+
+// $ go test -v -tags integration -args -dbengine postgres -dbhost 127.0.0.1 -dbport 5432 -dbuser username -dbname foodb -dbpass 12345
+var (
+	dbhost = flag.String("dbhost", "", "Database host.")
+	dbport = flag.String("dbport", "", "Database port.")
+	dbuser = flag.String("dbuser", "", "Database user.")
+	dbpass = flag.String("dbpass", "", "Database password.")
+	dbname = flag.String("dbname", "", "Database name.")
+)
+
+// TestDB test for open & close database.
+func TestDB(t *testing.T) {
+	conn := openDB(t)
+	closeDB(t, conn)
+}
+
+func openDB(t *testing.T) *pgx.Conn {
+	dbcfg := config.Config{
+		DBHost:     *dbhost,
+		DBPort:     *dbport,
+		DBUser:     *dbuser,
+		DBPassword: *dbpass,
+		DBName:     *dbname,
+	}
+
+	conn, err := newConn(dbcfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return conn
+}
+
+func closeDB(t *testing.T, db *pgx.Conn) {
+	if err := db.Close(context.Background()); err != nil {
+		t.Fatal(err)
+	}
+}
---
+++ pgsql/sqlc/user_test.go
@@ -0,0 +1,176 @@
+//go:build integration
+// +build integration
+
+package sqlc
+
+import (
+	"testing"
+
+	domain "github.com/adrianolmedo/genesis"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func TestCreateUser(t *testing.T) {
+	t.Cleanup(func() {
+		cleanUsersData(t)
+	})
+
+	conn := openDB(t)
+	defer closeDB(t, conn)
+
+	u := NewUser(conn)
+
+	input := &domain.User{
+		FirstName: "John",
+		LastName:  "Doe",
+		Email:     "example@gmail.com",
+		Password:  "1234567a",
+	}
+
+	if err := u.Create(input); err != nil {
+		t.Fatal(err)
+	}
+
+	got, err := u.ByID(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got.CreatedAt.IsZero() {
+		t.Error("expected created at")
+	}
+
+	if got.UpdatedAt != nil {
+		t.Error("unexpected updated at")
+	}
+
+	if got.DeletedAt != nil {
+		t.Error("unexpected deleted at")
+	}
+}
+
+func TestUserByLogin(t *testing.T) {
+	t.Cleanup(func() {
+		cleanUsersData(t)
+	})
+
+	conn := openDB(t)
+	defer closeDB(t, conn)
+	insertUsersData(t, conn)
+
+	tt := []struct {
+		name        string
+		inputEmail  string
+		inputPass   string
+		errExpected bool
+	}{
+		{
+			name:        "successful",
+			inputEmail:  "example@gmail.com",
+			inputPass:   "1234567a",
+			errExpected: false,
+		},
+		{
+			name:        "user-not-found",
+			inputEmail:  "example@hotmail.com",
+			inputPass:   "1234567a",
+			errExpected: true,
+		},
+	}
+
+	u := NewUser(conn)
+
+	for _, tc := range tt {
+		err := u.ByLogin(tc.inputEmail, tc.inputPass)
+		if (err != nil) != tc.errExpected {
+			t.Errorf("%s: ByLogin(%s, %s): unexpected error status: %v",
+				tc.name, tc.inputEmail, tc.inputPass, err)
+		}
+	}
+}
+
+func TestUpdateUser(t *testing.T) {
+	t.Cleanup(func() {
+		cleanUsersData(t)
+	})
+
+	conn := openDB(t)
+	defer closeDB(t, conn)
+	insertUsersData(t, conn)
+
+	input := domain.User{
+		ID:        1,
+		FirstName: "Adrián",
+		LastName:  "Olmedo",
+		Email:     "example@gmail.com",
+		Password:  "1234567a",
+	}
+
+	u := NewUser(conn)
+
+	if err := u.Update(input); err != nil {
+		t.Fatal(err)
+	}
+
+	got, err := u.ByID(input.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got.FirstName != input.FirstName {
+		t.Errorf("FirstName: want %s, got %s", input.FirstName, got.FirstName)
+	}
+
+	if got.LastName != input.LastName {
+		t.Errorf("LastName: want %s, got %s", input.LastName, got.LastName)
+	}
+
+	if got.CreatedAt.IsZero() {
+		t.Error("expected created at")
+	}
+
+	if got.UpdatedAt.IsZero() {
+		t.Error("expected updated at")
+	}
+
+	if got.DeletedAt != nil {
+		t.Error("unexpected deleted at")
+	}
+}
+
+func cleanUsersData(t *testing.T) {
+	conn := openDB(t)
+	defer closeDB(t, conn)
+
+	u := NewUser(conn)
+	err := u.DeleteAll()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func insertUsersData(t *testing.T, conn *pgx.Conn) {
+	//conn := openDB(t)
+	//defer closeDB(t, conn)
+
+	u := NewUser(conn)
+
+	if err := u.Create(&domain.User{
+		FirstName: "John",
+		LastName:  "Doe",
+		Email:     "example@gmail.com",
+		Password:  "1234567a",
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := u.Create(&domain.User{
+		FirstName: "Jane",
+		LastName:  "Roe",
+		Email:     "qwerty@hotmail.com",
+		Password:  "1234567b",
+	}); err != nil {
+		t.Fatal(err)
+	}
+}
