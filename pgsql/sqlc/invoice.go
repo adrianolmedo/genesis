844024a6bbb263b3fd66a4de1d289c07686ed93c
Adrian Olmedo <adrianolmedo.ve@gmail.com>
2025-07-09 22:30:00 -0400

    Remove unecessary dbgen.DBTX as dependency struct field

---
+++ pgsql/sqlc/invoice.go
@@ -0,0 +1,58 @@
+package sqlc
+
+import (
+	"context"
+	"fmt"
+
+	domain "github.com/adrianolmedo/genesis"
+	"github.com/adrianolmedo/genesis/pgsql/sqlc/dbgen"
+
+	"github.com/jackc/pgx/v5"
+)
+
+// Invoice repository.
+type Invoice struct {
+	db     *pgx.Conn      // or *pgxpool.Pool
+	q      *dbgen.Queries // for non-tx operations
+	header *InvoiceHeader
+	items  *InvoiceItem
+}
+
+// NewInvoice creates a new Invoice repository instance.
+// Since InvoiceHader, Invoice Item (or any other repository in question)
+// are closely related to Invoice, they are created as part of the Invoice structure.
+func NewInvoice(db *pgx.Conn) *Invoice {
+	q := dbgen.New(db)
+	return &Invoice{
+		db:     db,
+		q:      q,
+		header: NewInvoiceHeader(db),
+		items:  NewInvoiceItem(db),
+	}
+}
+
+func (i Invoice) Create(inv *domain.Invoice) error {
+	ctx := context.Background()
+
+	tx, err := i.db.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			tx.Rollback(ctx)
+		}
+	}()
+
+	// Create invoice header
+	if err := i.header.Create(tx, inv.Header); err != nil {
+		return fmt.Errorf("invoice header: %w", err)
+	}
+
+	// Create invoice items
+	if err := i.items.Create(tx, inv.Header.ID, inv.Items); err != nil {
+		return fmt.Errorf("invoice items: %w", err)
+	}
+
+	return tx.Commit(ctx)
+}
